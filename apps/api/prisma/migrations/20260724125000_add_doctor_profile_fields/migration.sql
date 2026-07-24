dd124aa3ed45f72ced7fd4ad64d0b0d18bef1712
anshit-arya <anshit.arya@flipkart.com>
2026-07-24 18:15:41 +0530

    Added location

---
+++ apps/api/prisma/migrations/20260724125000_add_doctor_profile_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable Doctor
+ALTER TABLE "Doctor" ADD COLUMN "specialization" TEXT;
+ALTER TABLE "Doctor" ADD COLUMN "languages" TEXT;
+ALTER TABLE "Doctor" ADD COLUMN "experience" INTEGER;
+ALTER TABLE "Doctor" ADD COLUMN "consultationFee" INTEGER;
+ALTER TABLE "Doctor" ADD COLUMN "roomNumber" TEXT;
